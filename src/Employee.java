b5ef1f079a078ae68c2fa418cd65525de70a8bb3
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-25 15:03:40 +0400

    Создание класса

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+public String fullName;
+public int salary;
+public int dept;
+
+    public Employee(String fullName, int salary, int dept) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.dept = dept;
+    }
+}
